328699c7fb7915ed060dda4aa2bbe858c041cff9
Evgenione <fallangel07021989@gmail.com>
2023-02-17 18:42:01 +0300

    add MetodArray

---
+++ MetodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {11, 12, 23, 54, 45, 36, 27, 18, 91, 10};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index+1;
+    index++;
+}
